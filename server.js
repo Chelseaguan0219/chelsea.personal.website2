d232552b51c33a5a408492eeb7d7da4931a28fde
Chelseaguan0219 <81730540+Chelseaguan0219@users.noreply.github.com>
2023-10-09 14:55:23 -0700

    modify

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const nodemailer = require("nodemailer");
 require('dotenv').config();
 const PORT = process.env.PORT || 8000;
-
+const path = require('path');
 
 const app = express();
 app.use(cors());
