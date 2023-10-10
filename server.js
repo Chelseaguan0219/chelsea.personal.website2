51f62af9b7bbeb5a78764d4a717eb9090c5cc043
Chelseaguan0219 <81730540+Chelseaguan0219@users.noreply.github.com>
2023-10-10 13:02:46 -0700

    update

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const cors = require("cors");
 const nodemailer = require("nodemailer");
 require('dotenv').config();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 const path = require('path');
 
 
@@ -23,9 +23,8 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use("/", router);
-app.use(express.static(path.resolve(__dirname, './build')));
-
-
+// app.use(express.static(path.resolve(__dirname, './build')));
+app.use(express.static(path.join(__dirname, '/build')));
 const contactEmail = nodemailer.createTransport({
     service: 'gmail',
     auth: {
@@ -68,9 +67,13 @@ router.post("/api/contact", (req, res) => {
   res.json({ code: 200, status: "Message Sent" });
 });
 });
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './build','index.html'));
-})
-app.listen(8000, () => {
+// app.get('*', (req, res) => {
+  // res.sendFile(path.resolve(__dirname, './build','index.html'));
+// 
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, '/build', 'index.html'));
+});
+
+app.listen(5000, () => {
     console.log(`Server is online on port: ${PORT}`)
 });
